2d7b97c4c838701b835580f394604da5e2b6b8b6
Zwillipapa <steinert.steven@googlemail.com>
2016-12-21 07:18:56 +0100

    Update craft.js

--- src/lang/en/craft.js
+++ src/lang/en/craft.js
@@ -1,9 +1,9 @@
 /* global Craft */
 
 Craft.help = {
-    "Stock": "Um einen Stock zu erhalten click auf einen dicken Ast und dann \"Entasten\" option.",
-    "Zweig": "Um einen Zweig zu erhalten click auf einen dünnen Ast und dann \"Entasten\" option.",
-    "Stein": "Stein kann mit einer Spitzhacke \"abgebaut\" werden von einem Steinhaufen oder Steinblock, oder du sammelst sie von der Erde auf",
-    "Leder": "Erst spanne rohes Fell auf einen Trockenrahmen. Dann benutze eine Gerbwanne um daraus Leder herzustellen." ,
+    "stick": "Um einen Stock zu erhalten click auf einen dicken Ast und dann \"Entasten\" option.",
+    "twig": "Um einen Zweig zu erhalten click auf einen dünnen Ast und dann \"Entasten\" option.",
+    "stone": "Stein kann mit einer Spitzhacke \"abgebaut\" werden von einem Steinhaufen oder Steinblock, oder du sammelst sie von der Erde auf",
+    "leather": "Erst spanne rohes Fell auf einen Trockenrahmen. Dann benutze eine Gerbwanne um daraus Leder herzustellen." ,
 
 };
